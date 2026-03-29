a2df0a284972c2f76bcd2cc286108de3a94654b5
Bruno Nascimento <sunr00t@gmail.com>
2026-03-28 21:16:46 -0300

    fix

--- components/About.js
+++ components/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import profile from '../public/profile.png';
+import profile from '../public/profile.jpg';
 
 const About = () => {
   return (
